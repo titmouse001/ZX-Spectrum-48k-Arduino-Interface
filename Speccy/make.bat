38fa6ad6bef96f38cdcbdfd3f2c3a8be505a4dca
Paul Overy <titmouse001@gmail.com>
2024-07-05 21:23:24 +0100

    Pre-Loading data
    Moved port usage from ISR to main loop.  The main loop now sets the port data in advance as the Z80 is now being halted after each IN.  This work really well and not seeing any data issue.  Have a out by 1 bug currently as the loop does not know about the last item, will fix next.  Getting this in now as it's tested works .

--- Speccy/make.bat
+++ Speccy/make.bat
@@ -1,10 +1,11 @@
 
-pasmo-0.5.4.beta2\pasmo -v --tapbas screenin.asm output\GET_waitFF.tap
+pasmo-0.5.4.beta2\pasmo -v --tapbas screenin.asm output\GET_wait8.tap
+pasmo-0.5.4.beta2\pasmo -v --tapbas screenTest.asm output\screenTest.tap
 
 REM pasmo-0.5.4.beta2\pasmo -v --bin screenin.asm output\screenin.bin
 rem pasmo-0.5.4.beta2\pasmo -v --tapbas screeninBW.asm output\screeninBW.tap
 rem Old Tests
 rem pasmo-0.5.4.beta2\pasmo -v --tapbas hi.asm output\hi.tap
-rem pasmo-0.5.4.beta2\pasmo -v --tapbas screenTest.asm output\screenTest.tap
+
 
 pause
