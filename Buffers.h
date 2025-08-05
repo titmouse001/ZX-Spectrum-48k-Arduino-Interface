712e5100e635e918ffa49411a6512933ae58da26
Paul Overy <titmouse001@gmail.com>
2025-08-05 19:32:19 +0100

    Replacing magic numbers
    Creating enum classes for all Packets types.

--- Buffers.h
+++ Buffers.h
@@ -3,7 +3,76 @@
 
 #include "constants.h"
 
-constexpr uint8_t COMMAND_ADDR_SIZE=2;
+#define E(e) static_cast<uint8_t>(e)
+
+enum class TransmitKeyPacket : uint8_t {
+  CMD_HIGH  = 0,
+  CMD_LOW   = 1,
+  CMD_DELAY = 2,
+  PACKET_LEN 
+};
+
+enum class ExecutePacket : uint8_t{
+  CMD_HIGH  = 0,
+  CMD_LOW   = 1,
+  PACKET_LEN 
+};
+
+enum class TransferPacket : uint8_t {
+  CMD_HIGH  = 0,
+  CMD_LOW   = 1,
+  CMD_LEN   = 2,
+  CMD_DEST_ADDR_HIGH   = 3,
+  CMD_DEST_ADDR_LOW  = 4,
+  PACKET_LEN 
+};
+
+enum class FillPacket : uint8_t {
+  CMD_HIGH  = 0,
+  CMD_LOW   = 1,
+  CMD_AMOUNT_HIGH = 2,
+  CMD_AMOUNT_LOW  = 3,
+  CMD_START_ADDR_HIGH = 4,
+  CMD_START_ADDR_LOW  = 5,
+  CMD_FILL_VALUE  = 6,
+  PACKET_LEN 
+};
+
+enum class SmallFillPacket : uint8_t {
+  CMD_HIGH  = 0,
+  CMD_LOW   = 1,
+  CMD_AMOUNT   = 2,
+  CMD_START_ADDR_HIGH = 3,
+  CMD_START_ADDR_LOW  = 4,
+  CMD_FILL_VALUE  = 5,
+  PACKET_LEN 
+};
+
+enum class StackPacket : uint8_t {
+  CMD_HIGH  = 0,
+  CMD_LOW   = 1,
+  CMD_SP_ADDR_HIGH = 2,
+  CMD_SP_ADDR_LOW  = 3,
+  PACKET_LEN 
+};
+
+
+enum class CopyPacket : uint8_t {
+  CMD_HIGH  = 0,
+  CMD_LOW   = 1,
+  CMD_LEN   = 2,
+  CMD_DEST_ADDR_HIGH   = 3,
+  CMD_DEST_ADDR_LOW  = 4,
+  PACKET_LEN 
+};
+
+
+enum class WaitPacket : uint8_t{
+  CMD_HIGH  = 0,
+  CMD_LOW   = 1,
+  PACKET_LEN 
+};
+
 
 // --- Buffer Segment Definitions ---
 constexpr uint16_t COMMAND_PAYLOAD_SECTION_SIZE = 255; 
@@ -13,7 +82,7 @@ constexpr uint16_t FILE_READ_BUFFER_OFFSET = 5 + COMMAND_PAYLOAD_SECTION_SIZE;
 constexpr uint16_t TOTAL_PACKET_BUFFER_SIZE = 5 + COMMAND_PAYLOAD_SECTION_SIZE + FILE_READ_BUFFER_SIZE;
 
 uint8_t packetBuffer[TOTAL_PACKET_BUFFER_SIZE];
-uint8_t head27_Execute[SNA_TOTAL_ITEMS + COMMAND_ADDR_SIZE ]; 
+uint8_t head27_Execute[SNA_TOTAL_ITEMS + static_cast<uint8_t>(ExecutePacket::PACKET_LEN) ]; 
 byte TextBuffer[SmallFont::FNT_BUFFER_SIZE * SmallFont::FNT_HEIGHT] = { 0 }; // SmallFont 5x7
 
 namespace Buffers {
@@ -76,55 +145,55 @@ void setupFunctions() {
 }
 
 static inline void buildTransferCommand(uint8_t* buf, uint16_t address, uint8_t length) {
-	buf[0] = (uint8_t)((command_Transfer) >> 8);
-	buf[1] = (uint8_t)((command_Transfer)&0xFF);
-	buf[2] = length;
-	buf[3] = (uint8_t)((address) >> 8);
-	buf[4] = (uint8_t)((address)&0xFF);
+	buf[static_cast<uint8_t>(TransferPacket::CMD_HIGH)] = (uint8_t)((command_Transfer) >> 8);
+	buf[static_cast<uint8_t>(TransferPacket::CMD_LOW)] = (uint8_t)((command_Transfer)&0xFF);
+	buf[static_cast<uint8_t>(TransferPacket::CMD_LEN)] = length;
+	buf[static_cast<uint8_t>(TransferPacket::CMD_DEST_ADDR_HIGH)] = (uint8_t)((address) >> 8);
+	buf[static_cast<uint8_t>(TransferPacket::CMD_DEST_ADDR_LOW)] = (uint8_t)((address)&0xFF);
 }
 
 static inline void buildSmallFillCommand(uint8_t* buf,uint16_t address, uint8_t runAmount, uint8_t value) {
-	buf[0] = (uint8_t)((command_SmallFill) >> 8);
-	buf[1] = (uint8_t)((command_SmallFill)&0xFF);
-	buf[2] = runAmount;
-	buf[3] = (uint8_t)((address) >> 8);
-	buf[4] = (uint8_t)((address)&0xFF);
-	buf[5] = value;
+	buf[static_cast<uint8_t>(SmallFillPacket::CMD_HIGH)] = (uint8_t)((command_SmallFill) >> 8);
+	buf[static_cast<uint8_t>(SmallFillPacket::CMD_LOW)] = (uint8_t)((command_SmallFill)&0xFF);
+	buf[static_cast<uint8_t>(SmallFillPacket::CMD_AMOUNT)] = runAmount;
+	buf[static_cast<uint8_t>(SmallFillPacket::CMD_START_ADDR_HIGH)] = (uint8_t)((address) >> 8);
+	buf[static_cast<uint8_t>(SmallFillPacket::CMD_START_ADDR_LOW)] = (uint8_t)((address)&0xFF);
+	buf[static_cast<uint8_t>(SmallFillPacket::CMD_FILL_VALUE)] = value;
 }
 
 static inline void buildStackCommand(uint8_t* buf,uint16_t address) {
-	buf[0] = (uint8_t)(command_Stack >> 8);
-	buf[1] = (uint8_t)(command_Stack & 0xFF);
-	buf[2] = (uint8_t)(address >> 8);
-	buf[3] = (uint8_t)(address & 0xFF);
+	buf[static_cast<uint8_t>(StackPacket::CMD_HIGH)] = (uint8_t)(command_Stack >> 8);
+	buf[static_cast<uint8_t>(StackPacket::CMD_LOW)] = (uint8_t)(command_Stack & 0xFF);
+	buf[static_cast<uint8_t>(StackPacket::CMD_SP_ADDR_HIGH)] = (uint8_t)(address >> 8);
+	buf[static_cast<uint8_t>(StackPacket::CMD_SP_ADDR_LOW)] = (uint8_t)(address & 0xFF);
 }
 
 static inline void buildCopyCommand(uint8_t* buf, uint16_t address, uint8_t length) {
-    buf[0] = (uint8_t)((command_Copy) >> 8);
-    buf[1] = (uint8_t)((command_Copy) & 0xFF);
-    buf[2] = length;
-    buf[3] = (uint8_t)((address) >> 8);
-    buf[4] = (uint8_t)((address) & 0xFF);
+    buf[static_cast<uint8_t>(CopyPacket::CMD_HIGH)] = (uint8_t)((command_Copy) >> 8);
+    buf[static_cast<uint8_t>(CopyPacket::CMD_LOW)] = (uint8_t)((command_Copy) & 0xFF);
+    buf[static_cast<uint8_t>(CopyPacket::CMD_LEN)] = length;
+    buf[static_cast<uint8_t>(CopyPacket::CMD_DEST_ADDR_HIGH)] = (uint8_t)((address) >> 8);
+    buf[static_cast<uint8_t>(CopyPacket::CMD_DEST_ADDR_LOW)] = (uint8_t)((address) & 0xFF);
 }
 
 static inline void buildFillCommand(uint8_t* buf, uint16_t amount, uint16_t address, uint8_t value) {
-    buf[0] = (uint8_t)((command_Fill) >> 8);
-    buf[1] = (uint8_t)((command_Fill) & 0xFF);
-    buf[2] = (uint8_t)((amount) >> 8);
-    buf[3] = (uint8_t)((amount) & 0xFF);
-    buf[4] = (uint8_t)((address) >> 8);
-    buf[5] = (uint8_t)((address) & 0xFF);
-    buf[6] = value;
+    buf[static_cast<uint8_t>(FillPacket::CMD_HIGH)] = (uint8_t)((command_Fill) >> 8);
+    buf[static_cast<uint8_t>(FillPacket::CMD_LOW)] = (uint8_t)((command_Fill) & 0xFF);
+    buf[static_cast<uint8_t>(FillPacket::CMD_AMOUNT_HIGH)] = (uint8_t)((amount) >> 8);
+    buf[static_cast<uint8_t>(FillPacket::CMD_AMOUNT_LOW)] = (uint8_t)((amount) & 0xFF);
+    buf[static_cast<uint8_t>(FillPacket::CMD_START_ADDR_HIGH)] = (uint8_t)((address) >> 8);
+    buf[static_cast<uint8_t>(FillPacket::CMD_START_ADDR_LOW)] = (uint8_t)((address) & 0xFF);
+    buf[static_cast<uint8_t>(FillPacket::CMD_FILL_VALUE)] = value;
 }
 
 static inline void buildWaitCommand(uint8_t* buf) {
-    buf[0] = (uint8_t)((command_Wait) >> 8);
-    buf[1] = (uint8_t)((command_Wait) & 0xFF);
+    buf[static_cast<uint8_t>(WaitPacket::CMD_HIGH)] = (uint8_t)((command_Wait) >> 8);
+    buf[static_cast<uint8_t>(WaitPacket::CMD_LOW)] = (uint8_t)((command_Wait) & 0xFF);
 }
 
 static inline void buildExecuteCommand(uint8_t* buf) {
-    buf[0] = (uint8_t)((command_Execute) >> 8);
-    buf[1] = (uint8_t)((command_Execute) & 0xFF);
+    buf[static_cast<uint8_t>(ExecutePacket::CMD_HIGH)] = (uint8_t)((command_Execute) >> 8);
+    buf[static_cast<uint8_t>(ExecutePacket::CMD_LOW)] = (uint8_t)((command_Execute) & 0xFF);
 }
 
 
--- Z80Bus.h
+++ Z80Bus.h
@@ -23,7 +23,7 @@
 
 namespace Z80Bus {
 
-constexpr uint8_t COMMAND_ADDR_SIZE=2;
+constexpr uint8_t COMMAND_ADDR_SIZE = 2;
 
 //-------------------------------------------------
 // Z80 Data Transfer and Control Routines
@@ -55,7 +55,7 @@ void setupPins() {
  * waitHalt:
  * Waits for Z80 HALT line to go LOW then HIGH (active low)
  * This shows the Z80 has resumed
-*/ 
+*/
 void waitHalt() {
   while (digitalReadFast(Pin::Z80_HALT) != 0) {};
   while (digitalReadFast(Pin::Z80_HALT) == 0) {};
@@ -88,72 +88,73 @@ void waitRelease_NMI() {
   while (digitalReadFast(Pin::Z80_HALT) == 0) {};
 }
 
-__attribute__((optimize("-Ofast"))) 
-void sendBytes(byte* data, uint16_t size) {
-// cli();  // Not really needed
+__attribute__((optimize("-Ofast"))) void sendBytes(byte* data, uint16_t size) {
+  // cli();  // Not really needed
   for (uint16_t i = 0; i < size; i++) {
     // Wait for Z80 HALT line to go LOW (active low)
     while (digitalReadFast(Pin::Z80_HALT) != 0) {};
     PORTD = data[i];  // Send Arduino d0-d7 to Z80 d0-d7
     // Pulse the Z80’s /NMI line: LOW -> HIGH to un-halt the CPU.
     digitalWriteFast(Pin::Z80_NMI, LOW);
-   //   asm volatile(
-   //     "nop\n nop\n nop\n nop\n nop\n nop\n nop\n nop\n"  // 8 NOPs = 500ns @ 16MHz
+    //   asm volatile(
+    //     "nop\n nop\n nop\n nop\n nop\n nop\n nop\n nop\n"  // 8 NOPs = 500ns @ 16MHz
     //    "nop\n nop\n nop\n nop\n nop\n nop\n nop\n nop\n"  // 16 total NOPs
     //  );
     digitalWriteFast(Pin::Z80_NMI, HIGH);
     // Wait for HALT line to return HIGH again (shows Z80 has resumed)
     while (digitalReadFast(Pin::Z80_HALT) == 0) {};
   }
- // sei();
+  // sei();
 }
 
 void sendSnaHeader(byte* header) {
-  // head27_2[0] contains COMMAND ADDRESS
-  Z80Bus::sendBytes(&header[0], COMMAND_ADDR_SIZE + 1 + 2 + 2 + 2 + 2);  // Send COMMAND ADDR then  I,HL',DE',BC',AF'
-  Z80Bus::sendBytes(&header[COMMAND_ADDR_SIZE + 15], 2 + 2 + 1 + 1);     // Send IY,IX,IFF2,R (packet data continued)
-  Z80Bus::sendBytes(&header[COMMAND_ADDR_SIZE + 23], 2);                 // Send SP                     "
-  Z80Bus::sendBytes(&header[COMMAND_ADDR_SIZE + 9], 2);                  // Send HL                     "
-  Z80Bus::sendBytes(&header[COMMAND_ADDR_SIZE + 25], 1);                 // Send IM                     "
-  Z80Bus::sendBytes(&header[COMMAND_ADDR_SIZE + 26], 1);                 // Send BorderColour           "
-  Z80Bus::sendBytes(&header[COMMAND_ADDR_SIZE + 11], 2);                 // Send DE                     "
-  Z80Bus::sendBytes(&header[COMMAND_ADDR_SIZE + 13], 2);                 // Send BC                     "
-  Z80Bus::sendBytes(&header[COMMAND_ADDR_SIZE + 21], 2);                 // Send AF                     "
+  constexpr uint8_t PKT_LEN = static_cast<uint8_t>(ExecutePacket::PACKET_LEN);
+  Z80Bus::sendBytes(&header[0], PKT_LEN + 1 + 2 + 2 + 2 + 2);  // Send COMMAND ADDR then  I,HL',DE',BC',AF'
+  Z80Bus::sendBytes(&header[PKT_LEN + 15], 2 + 2 + 1 + 1);     // Send IY,IX,IFF2,R (packet data continued)
+  Z80Bus::sendBytes(&header[PKT_LEN + 23], 2);                 // Send SP                     "
+  Z80Bus::sendBytes(&header[PKT_LEN + 9], 2);                  // Send HL                     "
+  Z80Bus::sendBytes(&header[PKT_LEN + 25], 1);                 // Send IM                     "
+  Z80Bus::sendBytes(&header[PKT_LEN + 26], 1);                 // Send BorderColour           "
+  Z80Bus::sendBytes(&header[PKT_LEN + 11], 2);                 // Send DE                     "
+  Z80Bus::sendBytes(&header[PKT_LEN + 13], 2);                 // Send BC                     "
+  Z80Bus::sendBytes(&header[PKT_LEN + 21], 2);                 // Send AF                     "
 }
 
 void fillScreenAttributes(const uint8_t attributes) {
   const uint16_t amount = 768;
   const uint16_t fillAddr = 0x5800;
-    Buffers::buildFillCommand(packetBuffer,amount,fillAddr, attributes);
-    Z80Bus::sendBytes(packetBuffer, 7);
+  Buffers::buildFillCommand(packetBuffer, amount, fillAddr, attributes);
+  Z80Bus::sendBytes(packetBuffer, 7);
 }
 
 void highlightSelection(uint16_t currentFileIndex, uint16_t startFileIndex, uint16_t& oldHighlightAddress) {
   const uint16_t amount = 32;
   const uint16_t fillAddr = 0x5800 + ((currentFileIndex - startFileIndex) * 32);
+  constexpr uint8_t BlackSelector = B01000111;
+  constexpr uint8_t CyanSelector = B00101000;
 
   if (oldHighlightAddress != fillAddr) {
-    Buffers::buildFillCommand(packetBuffer,amount,oldHighlightAddress, B01000111);
+    Buffers::buildFillCommand(packetBuffer, amount, oldHighlightAddress, BlackSelector);
     Z80Bus::sendBytes(packetBuffer, 7);
     oldHighlightAddress = fillAddr;
   }
-  Buffers::buildFillCommand(packetBuffer,amount,fillAddr, B00101000);
+  Buffers::buildFillCommand(packetBuffer, amount, fillAddr, CyanSelector);
   Z80Bus::sendBytes(packetBuffer, 7);
 }
 
-// TO DO - upgrade to use 6bit binary 
+// TO DO - upgrade to use 6bit binary
 uint8_t GetKeyPulses() {
 
-  constexpr uint8_t DELAY_ITERATIONS_PARAM= 20;  // 20 loops of 25 t-states
+  constexpr uint8_t DELAY_ITERATIONS_PARAM = 20;  // 20 loops of 25 t-states
   constexpr uint16_t PULSE_TIMEOUT_US = 70;
+
+  packetBuffer[static_cast<uint8_t>(TransmitKeyPacket::CMD_HIGH)] = (uint8_t)((Buffers::command_TransmitKey) >> 8);
+  packetBuffer[static_cast<uint8_t>(TransmitKeyPacket::CMD_LOW)] = (uint8_t)((Buffers::command_TransmitKey)&0xFF);
+  packetBuffer[static_cast<uint8_t>(TransmitKeyPacket::CMD_DELAY)] = DELAY_ITERATIONS_PARAM;  // delay use as end marker
+  Z80Bus::sendBytes(packetBuffer, static_cast<uint8_t>(TransmitKeyPacket::PACKET_LEN));
+
   uint8_t pulseCount = 0;
   uint32_t lastPulseTime = 0;
-
-  packetBuffer[0] = (uint8_t)((Buffers::command_TransmitKey) >> 8);
-  packetBuffer[1] = (uint8_t)((Buffers::command_TransmitKey)&0xFF);
-  packetBuffer[2] = DELAY_ITERATIONS_PARAM;  // delay use as end marker
-  Z80Bus::sendBytes(packetBuffer, 3);
-
   while (1) {                          // Tally halts, noting the end marker is just a time gap
     if ((PINB & (1 << PINB0)) == 0) {  // halt active
       // Signal the speccy to un-halt the Z80 - Pulse the Z80’s /NMI line: LOW -> HIGH
