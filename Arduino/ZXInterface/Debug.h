81aa6205c05b6fd3948c7f20934d29af7d1e3983
Paul Overy <titmouse001@gmail.com>
2026-07-30 21:18:19 +0100

    removed DEBUG_OLED

--- Arduino/ZXInterface/Debug.h
+++ Arduino/ZXInterface/Debug.h
@@ -1,19 +1,7 @@
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
 
-// ----------------------------------------------------------------------------------
-// Put these back to enable debug options (I've - not used this in ages - should work!!!)
-
-//#define DEBUG_OLED   --- REMOVED ,  NO LONGER SUPPORTED
-//#define SERIAL_DEBUG
-
-// Having both these enabled will EAT your flash program memory!!!
-// (reduce buffer sizes for testing)
-
-// ----------------------------------------------------------------------------------
-
 namespace Debug {
- //   bool setupOled();  NO LONGER SUPPORTED
     void setupSerial();
 }
 
